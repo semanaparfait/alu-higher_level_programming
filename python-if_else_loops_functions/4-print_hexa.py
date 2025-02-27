b04fdd0117415e50a8627a956d0df07f21d32ccf
semanaparfait <p.semana@alustudent.com>
2025-02-27 01:57:43 -0800

    a program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for num in range(97, 123):
-if num == 101 or num == 113:
-continue
-print("{}".format(chr(num)), end="")
+for num in range(99):
+    print("{} = {}".format(num, hex(num)))
